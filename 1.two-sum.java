fce890ecfe1c44ea4e2a26c65221a7c2d8cdb279
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-06-29 15:05:33 +0530

    hashmap approach

--- 1.two-sum.java
+++ 1.two-sum.java
@@ -5,16 +5,25 @@
  */
 
 // @lc code=start
+
+import java.util.HashMap;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[] { i, j };
-                }
+
+            int difference = target - nums[i];
+
+            if (map.containsKey(difference)) {
+                return new int[] { map.get(difference), i };
             }
+
+            map.put(nums[i], i);
         }
-        return new int[] {};
+
+        return new int[] {}; // Return an empty array if no solution is found
     }
 }
 // @lc code=end
