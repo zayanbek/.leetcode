382e0251fd85821e5bb6c8d6f3d8f73b88f0433a
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-06-30 18:23:56 +0530

    sorting char array

--- 242.valid-anagram.java
+++ 242.valid-anagram.java
@@ -5,33 +5,18 @@
  */
 
 // @lc code=start
+
+import java.util.Arrays;
+
 class Solution {
     public boolean isAnagram(String s, String t) {
 
-        int[] count = new int[26];
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
 
-        // Check if lengths don't match
-        if (s.length() != t.length()) {
-            return false;
-        }
-
-        // Count how many of each char
-        for (int i = 0; i < s.length(); i++) {
-            count[s.charAt(i) - 'a']++;
-        }
-
-        for (int i = 0; i < t.length(); i++) {
-            count[t.charAt(i) - 'a']--;
-        }
-
-        // Compare character counts
-        for (int i = 0; i < 26; i++) {
-            if (count[i] != 0) {
-                return false;
-            }
-        }
-
-        return true;
+        return new String(sArray).equals(new String(tArray));
     }
 }
 // @lc code=end
