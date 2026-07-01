97be0764c5c3c89f87950242baf40d43a04d25bc
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-07-02 00:07:50 +0530

    after google

--- 347.top-k-frequent-elements.py
+++ 347.top-k-frequent-elements.py
@@ -13,15 +13,20 @@ class Solution(object):
         :rtype: List[int]
         """
         
-        frequencies = {}
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
         for num in nums:
-            if num in frequencies:
-                frequencies[num] += 1
-            else:
-                frequencies[num] = 1
-                
-        sorted_frequencies = sorted(frequencies.items(), key=lambda x: x[1], reverse=True)
-        return [item[0] for item in sorted_frequencies[:k]]
+            count[num] = 1 + count.get(num, 0)
+        for num, cnt in count.items():
+            freq[cnt].append(num)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
         
 # @lc code=end
 
