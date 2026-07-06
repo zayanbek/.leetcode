2ea1f3a9bb11b02cd8a23527a9486a007b1f2bb7
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-07-06 17:42:45 +0530

    two pointer solution

--- 11.container-with-most-water.py
+++ 11.container-with-most-water.py
@@ -12,15 +12,27 @@ class Solution(object):
         :rtype: int
         """
         
+        left = 0
+        right = len(height) - 1
+        
         max_area = 0
         
-        for i in range(len(height)):
-            for j in range(i + 1, len(height)):
-                
-                area = min(height[i], height[j]) * (j - i)
-                max_area = max(max_area, area)
+        while left < right:
+            
+            area = min(height[left], height[right]) * (right - left)
+            max_area = max(max_area, area)
+            
+            if height[left] <= height[right]:
+                left += 1
+            else:
+                right -= 1
+              
+            #left += 1
+            #right -= 1
         
         return max_area
+        
+        
     
 # @lc code=end
 
