0b9f0ff26339a4a9a40fcb62a3d51500d8697bd9
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-07-13 14:02:18 +0530

    start

---
+++ 853.car-fleet.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=853 lang=python
+#
+# [853] Car Fleet
+#
+
+# @lc code=start
+class Solution(object):
+    def carFleet(self, target, position, speed):
+        """
+        :type target: int
+        :type position: List[int]
+        :type speed: List[int]
+        :rtype: int
+        """
+        
+# @lc code=end
