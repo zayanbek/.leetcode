42de51c10c57280ad138c6656ffcfd849cb2e66f
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-07-18 20:29:23 +0530

    this solution may exit earlier than O(n)

--- 217.contains-duplicate.py
+++ 217.contains-duplicate.py
@@ -14,7 +14,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        return len(nums) != len(set(nums))
+        seen = set()
+
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+
+        return False
         
 # @lc code=end
 
