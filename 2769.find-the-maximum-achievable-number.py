ada2ac58ac4116c0edc7b0a91b6e46f9244066d5
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-08-03 21:16:34 -0500

    how is the memory usage only 16.63% ??

---
+++ 2769.find-the-maximum-achievable-number.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=2769 lang=python
+#
+# [2769] Find the Maximum Achievable Number
+#
+
+# @lc code=start
+class Solution(object):
+    def theMaximumAchievableX(self, num, t):
+        """
+        :type num: int
+        :type t: int
+        :rtype: int
+        """
+        
+        return num + 2 * t
+
+        
+# @lc code=end
+
