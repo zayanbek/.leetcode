4a566f14914b18c9b93f562a6cc4224150631725
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-08-07 10:07:29 -0500

    doing this problem with recursive dfs is so much easier

--- 226.invert-binary-tree.java
+++ 226.invert-binary-tree.java
@@ -23,16 +23,17 @@
 class Solution {
     public TreeNode invertTree(TreeNode root) {
         if (root == null) return null;
-        Stack<TreeNode> stack = new Stack<>();
-        stack.push(root);
-        while (!stack.isEmpty()) {
-            TreeNode node = stack.pop();
-            TreeNode temp = node.left;
-            node.left = node.right;
-            node.right = temp;
-            if (node.left != null) stack.push(node.left);
-            if (node.right != null) stack.push(node.right);
-        }
+
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+
         return root;
     }
 }
