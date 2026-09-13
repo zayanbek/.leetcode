f2654fbd8a4aeb460f1790bb5b72a353dbe78cba
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-09-13 10:43:31 -0500

    dynamic programming melts my brain

---
+++ 322.coin-change.java
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=322 lang=java
+ *
+ * [322] Coin Change
+ */
+
+// @lc code=start
+public class Solution {
+    HashMap<Integer, Integer> memo = new HashMap<>();
+
+    public int dfs(int amount, int[] coins) {
+        if (amount == 0) return 0;
+        if (memo.containsKey(amount))
+            return memo.get(amount);
+
+        int res = Integer.MAX_VALUE;
+        for (int coin : coins) {
+            if (amount - coin >= 0) {
+                int result = dfs(amount - coin, coins);
+                if (result != Integer.MAX_VALUE) {
+                    res = Math.min(res, 1 + result);
+                }
+            }
+        }
+
+        memo.put(amount, res);
+        return res;
+    }
+
+    public int coinChange(int[] coins, int amount) {
+        int minCoins = dfs(amount, coins);
+        return minCoins == Integer.MAX_VALUE ? -1 : minCoins;
+    }
+}
+
+// @lc code=end
+
